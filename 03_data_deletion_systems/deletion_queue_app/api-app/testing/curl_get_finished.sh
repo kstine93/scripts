d7eb428ead4a97576b491d5a3a8f46b198c7b596
Kevin Stine <kevin.stine@zalando.de>
2023-02-02 18:42:14 +0100

    finished v1 of class methods, built more of api endpoints

---
+++ 03_data_deletion_systems/deletion_queue_app/api-app/testing/curl_get_finished.sh
@@ -0,0 +1,2 @@
+#Make API request adding a new request to the system:
+curl -X POST 127.0.0.1:5000/api/v1/requests/finished
--- 03_data_deletion_systems/deletion_queue_app/api-app/testing/curl_new_requests.sh
+++ 03_data_deletion_systems/deletion_queue_app/api-app/testing/curl_new_requests.sh
@@ -1,3 +1,3 @@
 #Make API request adding a new request to the system:
 curl -X POST -H "Content-Type: application/json" 127.0.0.1:5000/api/v1/requests/new \
--d '[{"email":"test@email.de","cause":"direct_request"}]'
+-d '[{"email":"test@email.de","cause":"direct_request"},{"email":"test2@email.de","cause":"direct_request"}]'
